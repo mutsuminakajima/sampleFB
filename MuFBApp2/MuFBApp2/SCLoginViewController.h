f4ebb1eafab9c35c3bdf0bbb3591f5e6260b8e9c
nakajima-m <nakajima-m@dhcp10-216.klab.org>
2013-02-04 17:28:47 +0900

    addLoginview

---
+++ MuFBApp2/MuFBApp2/SCLoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  SCLoginViewController.h
+//  MuFBApp2
+//
+//  Created by nakajima-m on 13/02/04.
+//  Copyright (c) 2013年 nakajima-m. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SCLoginViewController : UIViewController
+
+@end
---
+++ MuFBApp2/MuFBApp2/SCLoginViewController.m
@@ -0,0 +1,38 @@
+//
+//  SCLoginViewController.m
+//  MuFBApp2
+//
+//  Created by nakajima-m on 13/02/04.
+//  Copyright (c) 2013年 nakajima-m. All rights reserved.
+//
+
+#import "SCLoginViewController.h"
+
+@interface SCLoginViewController ()
+
+@end
+
+@implementation SCLoginViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
