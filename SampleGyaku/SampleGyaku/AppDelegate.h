ac4c6125ceff4a35282d5e74e99ce9ba7967e322
nakajima-m <nakajima-m@dhcp10-216.klab.org>
2013-02-08 14:43:06 +0900

    newproject

---
+++ SampleGyaku/SampleGyaku/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  SampleGyaku
+//
+//  Created by nakajima-m on 13/02/08.
+//  Copyright (c) 2013年 nakajima-m. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ SampleGyaku/SampleGyaku/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SampleGyaku
+//
+//  Created by nakajima-m on 13/02/08.
+//  Copyright (c) 2013年 nakajima-m. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
